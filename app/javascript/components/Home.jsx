75b216590290477d76c696fac42cc5ef4580856b
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-02-04 14:37:36 +0530

    Set up React Frontend jsx file

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Progress Tracker</h1>
+        <p className="lead">
+          An app to track your course progress.
+        </p>
+        <hr className="my-4" />        
+      </div>
+    </div>
+  </div>
+);
