c3970a8d32e235b8b9f305f0e6c8c3c1633179ae
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-02-05 14:00:31 +0530

    Implement handlers to input fields

--- app/javascript/components/Signup.jsx
+++ app/javascript/components/Signup.jsx
@@ -1,86 +1,154 @@
 import React from 'react';
 import Signin from './Signin';
 
-const Signup = () => (
-  <div className="container text-content sign-in-up">
-    <div className="row justify-content-center">
-      <div className="col-md-4 col-md-offset-4">
-        <br />
-        {/* Nav tabs */}
-        <div className="text-center">
-          <div className="btn-group">
-            <a href="#new" role="tab" data-toggle="tab" className="big btn btn-primary">
-              <i className="fa fa-plus" />
-              {' '}
-              Create Account
-            </a>
-            <a href="#user" role="tab" data-toggle="tab" className="big btn btn-success">
-              <i className="fa fa-user" />
-              {' '}
-              Login
-            </a>
-          </div>
-        </div>
-        <p className="click2select">Tap to select</p>
-        <div className="tab-content">
-          <div className="tab-pane fade in active" id="new">
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      units: '',
+      target: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const {
+      name, email, password, units, target,
+    } = this.state;
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  }
+
+  render() {
+    const {
+      name, email, password, units, target,
+    } = this.state;
+    return (
+      <div className="container text-content sign-in-up">
+        <div className="row justify-content-center">
+          <div className="col-md-4 col-md-offset-4">
             <br />
-            <fieldset>
-              <div className="form-group">
-                <div className="right-inner-addon">
-                  <i className="fa fa-envelope" />
-                  <input className="form-control input-lg" placeholder="Email" type="text" />
-                </div>
+            {/* Nav tabs */}
+            <div className="text-center">
+              <div className="btn-group">
+                <a href="#new" role="tab" data-toggle="tab" className="big btn btn-primary">
+                  <i className="fa fa-plus" />
+                  {' '}
+                  Create Account
+                </a>
+                <a href="#user" role="tab" data-toggle="tab" className="big btn btn-success">
+                  <i className="fa fa-user" />
+                  {' '}
+                  Login
+                </a>
               </div>
-              <div className="form-group">
-                <div className="right-inner-addon">
-                  <i className="fa fa-key" />
-                  <input className="form-control input-lg" placeholder="Password" type="password" />
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="right-inner-addon">
-                  <i className="fa fa-key" />
-                  <input className="form-control input-lg" placeholder="Confirm Password" id="" type="password" />
-                </div>
-              </div>
-              <div className="form-group">
-                <select className="custom-select custom-select-md">
-                  <option selected>Current course progress</option>
-                  <option value="10">10%</option>
-                  <option value="20">20%</option>
-                  <option value="30">30%</option>
+            </div>
+            <p className="click2select">Tap to select</p>
+            <div className="tab-content">
+              <div className="tab-pane fade in active" id="new">
+                <br />
+                <fieldset>
+                  <div className="form-group">
+                    <div className="right-inner-addon">
+                      <i className="fa fa-user" />
+                      <input
+                        className="form-control input-lg"
+                        placeholder="Name"
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={e => this.handleChange(e)}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="right-inner-addon">
+                      <i className="fa fa-envelop" />
+                      <input
+                        className="form-control input-lg"
+                        placeholder="Email"
+                        type="text"
+                        id="email"
+                        value={email}
+                        onChange={e => this.handleChange(e)}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="right-inner-addon">
+                      <i className="fa fa-key" />
+                      <input
+                        className="form-control input-lg"
+                        placeholder="Password"
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={e => this.handleChange(e)}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <select
+                      className="custom-select custom-select-md"
+                      id="units"
+                      value={units}
+                      onChange={e => this.handleChange(e)}
+                    >
+                      <option defaultValue>Select current progress</option>
+                      <option value="10">10%</option>
+                      <option value="20">20%</option>
+                       <option value="30">30%</option>
                   <option value="40">40%</option>
                   <option value="50">50%</option>
-                </select>                  
-              </div>
-              <div className="form-group">
-                <select className="custom-select custom-select-md">
-                  <option selected>Target progress at EOD</option>
+                    </select>
+                  </div>
+                  <div className="form-group">
+                    <select
+                      className="custom-select custom-select-md"
+                      id="target"
+                      value={target}
+                      onChange={e => this.handleChange(e)}
+                    >
+                      <option selected>Target progress at EOD</option>
+                  <option value="15">15%</option>
                   <option value="15">15%</option>                  
                   <option value="25">25%</option>
                   <option value="45">45%</option>
                   <option value="65">65%</option>
-                  <option value="75">75%</option>
-                </select>
+                    </select>
+                  </div>
+                </fieldset>
+                <hr />
+                <div className="tab-content">
+                  <div className="tab-pane active text-center" id="pp">
+                    <button
+                      className="btn btn-primary btn-lg btn-block"
+                      type="button"
+                      onClick={e => this.onSubmit(e)}
+                    >
+                      <i className="fa fa-plus" />
+                      {' '}
+                      Create Account
+                    </button>
+                  </div>
+                </div>
               </div>
-            </fieldset>
-            <hr />
-            <div className="tab-content">
-              <div className="tab-pane active text-center" id="pp">
-                <button type="button" className="btn btn-primary btn-lg btn-block">
-                  <i className="fa fa-plus" />
-                  {' '}
-                  Create Account
-                </button>
-              </div>
+              <Signin />
             </div>
           </div>
-          <Signin />
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Signup;
