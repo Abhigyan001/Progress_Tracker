cef682e32260fca301086f8d61f649dd54b144ae
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-02-05 13:54:23 +0530

    Redesign Authentication Interface

--- app/javascript/components/Signup.jsx
+++ app/javascript/components/Signup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Signin from './Signin';
 
 const Signup = () => (
   <div className="container text-content sign-in-up">
@@ -10,10 +11,12 @@ const Signup = () => (
           <div className="btn-group">
             <a href="#new" role="tab" data-toggle="tab" className="big btn btn-primary">
               <i className="fa fa-plus" />
+              {' '}
               Create Account
             </a>
             <a href="#user" role="tab" data-toggle="tab" className="big btn btn-success">
               <i className="fa fa-user" />
+              {' '}
               Login
             </a>
           </div>
@@ -49,12 +52,12 @@ const Signup = () => (
                   <option value="30">30%</option>
                   <option value="40">40%</option>
                   <option value="50">50%</option>
-                </select>
+                </select>                  
               </div>
               <div className="form-group">
                 <select className="custom-select custom-select-md">
                   <option selected>Target progress at EOD</option>
-                  <option value="15">15%</option>
+                  <option value="15">15%</option>                  
                   <option value="25">25%</option>
                   <option value="45">45%</option>
                   <option value="65">65%</option>
@@ -67,35 +70,13 @@ const Signup = () => (
               <div className="tab-pane active text-center" id="pp">
                 <button type="button" className="btn btn-primary btn-lg btn-block">
                   <i className="fa fa-plus" />
+                  {' '}
                   Create Account
                 </button>
               </div>
             </div>
           </div>
-          <div className="tab-pane fade" id="user">
-            <br />
-            <fieldset>
-              <div className="form-group">
-                <div className="right-inner-addon">
-                  <i className="fa fa-envelope" />
-                  <input className="form-control input-lg" placeholder="Email" type="text" />
-                </div>
-              </div>
-              <div className="form-group">
-                <div className="right-inner-addon">
-                  <i className="fa fa-key" />
-                  <input className="form-control input-lg" placeholder="Password" type="password" />
-                </div>
-              </div>
-            </fieldset>
-            <br />
-            <div className=" text-center">
-              <button type="button" className="btn btn-primary btn-success">
-                <i className="fa fa-user" />
-                LOGIN
-              </button>
-            </div>
-          </div>
+          <Signin />
         </div>
       </div>
     </div>
