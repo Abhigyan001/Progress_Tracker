704eaf3c4a3561147c66ac70343b4fb6646f6786
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-02-06 20:54:03 +0530

    Modify dependent destroy

--- db/migrate/20200913155728_create_readings.rb
+++ db/migrate/20200913155728_create_readings.rb
@@ -1,16 +1,11 @@
 class CreateReadings < ActiveRecord::Migration[6.0]
   def change
     create_table :readings do |t|
-      t.integer "bedroom"
-      t.integer "study"
-      t.integer "garage"
-      t.integer "living"
-      t.integer "kitchen"
-      t.integer "guest"
-      t.integer "consumption"
-      t.integer "available"
-      t.integer "saved"
-
+      t.integer "html"
+      t.integer "ruby"
+      t.integer "javascript"
+      t.integer "rubyonrails"
+      t.integer "react"
       t.timestamps
     end
   end
